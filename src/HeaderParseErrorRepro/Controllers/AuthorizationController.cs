8a0dd3b16585173443bb88b4b00d123bc2c1c50b
muhammed baykal <medeni@muhammedbaykal.com>
2016-06-19 03:47:07 +0300

    Repro

---
+++ src/HeaderParseErrorRepro/Controllers/AuthorizationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HeaderParseErrorRepro.Controllers
+{
+    [Route("services/[controller]")]
+    public class AuthorizationController : Controller
+    {
+        [Route("")]
+        public string InitScram([FromHeader(Name = "SCRAM-SHA-256")]string message)
+        {
+            return message ?? "<NIL>";
+        }
+    }
+}
---
+++ src/HeaderParseErrorRepro/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HeaderParseErrorRepro
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage(new DeveloperExceptionPageOptions() { SourceCodeLineCount = 20 })
+               .UseStatusCodePages()
+               .UseMvc();
+        }
+    }
+}
